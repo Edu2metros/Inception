9507f7975771e0d7f4aa26135a8f3cf19244fe19
Edu2metros <eduardosantos14.gomes@gmail.com>
2024-08-18 20:51:57 -0300

    ta quase

---
+++ srcs/requirements/wordpress/conf/auto-config.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ -f /var/www/wordpress/wp-config-sample.php ]; then
+    rm -rf /var/www/wordpress/wp-config-sample.php
+
+    # Verifica se o WordPress já está instalado
+    if ! wp core is-installed --allow-root --path=/var/www/wordpress; then
+        wp core install --allow-root \
+            --path=/var/www/wordpress \
+            --title="Inception" \
+            --url="$DOMAIN_NAME" \
+            --admin_user="$WP_ROOT_USER" \
+            --admin_password="$WP_ROOT_PASSWORD" \
+            --admin_email="$WP_ROOT_EMAIL"
+    fi
+
+    # Verifica se o usuário já existe antes de criar
+    if ! wp user get "$WP_USER" --allow-root --path=/var/www/wordpress > /dev/null 2>&1; then
+        wp user create --allow-root \
+            --path=/var/www/wordpress \
+            "$WP_USER" "$WP_EMAIL" \
+            --user_pass="$WP_PASSWORD" \
+            --role='author'
+    fi
+fi
+
+exec php-fpm -F
